fadd7a2d76d8f663fcdb9c82c75fbcc21743c976
daijirotakeuchi <mr.daijiro0517@gmail.com>
2018-02-17 08:19:30 +0000

    repair

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,9 +10,9 @@ class FavoritesController < ApplicationController
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
-    current_user.unmicropost(micropost)
+    current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to back
+    redirect_to :back
   end
 end
 
